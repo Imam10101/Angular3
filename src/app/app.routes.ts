774e040de32aaf4de1698cc5963b3c73ab63efaf
WADA <WADA@DESKTOP-3AU5U1E>
2024-04-24 19:11:20 +0600

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
+
+export const routes: Routes = [
+    {path:"reactive-form", component: ReactiveFormComponent }
+];
